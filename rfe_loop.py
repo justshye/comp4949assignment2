783578cf3aa61d1ce6c097e7239bdaad3b37f128
shye <113232835+justshye@users.noreply.github.com>
2024-04-14 14:41:02 -0700

    Restyle code

--- rfe_loop.py
+++ rfe_loop.py
@@ -14,7 +14,9 @@ X = data.drop(columns=["blueWin"])
 y = data["blueWin"]
 
 # Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Initialize the model
 model = LogisticRegression(max_iter=2000)
--- training.py
+++ training.py
@@ -8,11 +8,18 @@ from sklearn.metrics import accuracy_score
 data = pd.read_csv("match_data_v5.csv")
 
 # Define features and target variable
-features = ['blueTeamTotalKills', 'blueTeamDragonKills', 'redTeamDragonKills', 'redTeamTotalKills']
-target = 'blueWin'
+features = [
+    "blueTeamTotalKills",
+    "blueTeamDragonKills",
+    "redTeamDragonKills",
+    "redTeamTotalKills",
+]
+target = "blueWin"
 
 # Splitting the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    data[features], data[target], test_size=0.2, random_state=42
+)
 
 # Initializing the Logistic Regression model
 model = LogisticRegression(max_iter=2000)
